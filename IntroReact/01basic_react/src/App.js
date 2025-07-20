bbc8c1c40f6f4ad91bdfda55dca9b03363aef1d3
Omm Prakash Satapathy <ommprakshsatapathy@gmail.com>
2025-07-20 16:07:02 +0530

    just push

---
+++ IntroReact/01basic_react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>chai aur react | Omm Prakash Satapthy | just get knwo about the first project and get the details of some command and how to run react and vit project, how to config and all</h1>
+  );
+}
+
+export default App;
